806712591aeaff6454f72f35929ce5c6e88df130
zaracarr <zara.carr@gmail.com>
2025-08-05 07:11:01 -0500

    Update streamlit_app.py
    oandas

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@
 import streamlit as st
 from snowflake.snowpark.functions import col
 import requests
+import pandas as pd
 # Write directly to the app
 st.title(f":cup_with_straw: Customize Your Smoothie! :cup_with_straw:")
 st.write(
